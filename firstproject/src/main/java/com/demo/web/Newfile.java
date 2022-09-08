b3afcf7848a7300442e960f9e945cd596730f4d8
Purnendu <Purnendu@Purnendu-PC>
2022-09-08 09:41:43 +0530

    new test commit

---
+++ firstproject/src/main/java/com/demo/web/Newfile.java
@@ -0,0 +1,15 @@
+package com.demo.web;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class Newfile {
+	
+	public static void main(String[] args) {
+	
+		
+		System.out.println("Just test");
+	}
+	
+
+}
